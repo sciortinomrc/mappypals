daf3211ecd578ced0a4bae33ae60671b360a5293
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-16 10:52:25 +0000

    Deploy zero-to-mastery/mappypals to github.com/zero-to-mastery/mappypals.git:gh-pages

---
+++ precache-manifest.c1eb0141b0703e6fb4bec885d754e81d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af9979aa237e7f0bb39a9d1ab6c39b58",
+    "url": "/mappypals/index.html"
+  },
+  {
+    "revision": "a0ffcc6286b943d0acf6",
+    "url": "/mappypals/static/css/main.f0229dd3.chunk.css"
+  },
+  {
+    "revision": "ff6723800109cde06569",
+    "url": "/mappypals/static/js/2.5f61d441.chunk.js"
+  },
+  {
+    "revision": "a0ffcc6286b943d0acf6",
+    "url": "/mappypals/static/js/main.801ab159.chunk.js"
+  },
+  {
+    "revision": "c57707e0e7c42496a68c",
+    "url": "/mappypals/static/js/runtime~main.11b2500f.js"
+  },
+  {
+    "revision": "92aef774a76eac8d71011d32c08e5c40",
+    "url": "/mappypals/static/media/AboutUs.92aef774.svg"
+  },
+  {
+    "revision": "2a233ab4f7f74c306b1b4c85267f4b69",
+    "url": "/mappypals/static/media/FriendsIcon.2a233ab4.svg"
+  },
+  {
+    "revision": "ffd43200aed63b650fda658d8112c33c",
+    "url": "/mappypals/static/media/InviteFriends.ffd43200.svg"
+  },
+  {
+    "revision": "b657997bb9fda684697f996a388cae92",
+    "url": "/mappypals/static/media/Logo.b657997b.svg"
+  },
+  {
+    "revision": "9ff0000d12d89426a4ae8c5fedf7861f",
+    "url": "/mappypals/static/media/LogoInverted.9ff0000d.svg"
+  },
+  {
+    "revision": "75546895312232ce68d4079bb3ed6da4",
+    "url": "/mappypals/static/media/MySettingsIcon.75546895.svg"
+  },
+  {
+    "revision": "9b70621857433b6c4c9ff422355104d4",
+    "url": "/mappypals/static/media/Team.9b706218.svg"
+  },
+  {
+    "revision": "d96e6a53013e7a91058441e68ffa0e52",
+    "url": "/mappypals/static/media/adult-backlit-beach.d96e6a53.jpg"
+  }
+]);
